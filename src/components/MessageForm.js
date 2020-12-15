6b4cdb3a5684ce4f7ea9df94bd8f61d56d98aecd
pvahanian <51976194+pvahanian@users.noreply.github.com>
2020-12-14 21:45:19 -0600

    Fixed send button and text area.  Added enter function

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -3,19 +3,13 @@ import { AuthContext } from "./Auth"
 
 import 'firebase/firestore'
 import firebase from 'firebase/app';
-import { makeStyles, withStyles} from "@material-ui/core/styles";
-import FormatBoldIcon from '@material-ui/icons/FormatBold';
-import FormatItalicIcon from '@material-ui/icons/FormatItalic';
-import FormatUnderlinedIcon from '@material-ui/icons/FormatUnderlined';
+import { makeStyles} from "@material-ui/core/styles";
+
 import Paper from '@material-ui/core/Paper';
-import ToggleButton from '@material-ui/lab/ToggleButton';
-import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
 import TextField from "@material-ui/core/TextField";
 import Button from '@material-ui/core/Button';
 import SendIcon from '@material-ui/icons/Send';
-import AttachFileIcon from '@material-ui/icons/AttachFile';
-import CodeIcon from "@material-ui/icons/Code";
-import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
+
 
 import fireApp from '../base.js'
 const firestore = fireApp.firestore();
@@ -26,7 +20,8 @@ const useStyles = makeStyles((theme) => ({
         margin: theme.spacing(1),
         display:'flex',
         alignItems:'right',
-        flexDirection:'row-reverse'
+        flexDirection:'row-reverse',
+        width: "7%"
       },
   paper: {
     display: 'flex',
@@ -35,34 +30,17 @@ const useStyles = makeStyles((theme) => ({
   },
   root: {
     "& .MuiTextField-root": {
-      margin: theme.spacing(1),
-      width: "60ch"
+      margin: theme.spacing(.5),
+      width: "40%"
     }
   }
 }));
-const StyledToggleButtonGroup = withStyles((theme) => ({
-  grouped: {
-    margin: theme.spacing(0.5),
-    border: 'none',
-    '&:not(:first-child)': {
-      borderRadius: theme.shape.borderRadius,
-    },
-    '&:first-child': {
-      borderRadius: theme.shape.borderRadius,
-    },
-  },
-}))(ToggleButtonGroup);
 
 export default function MessageForm() {
   const messagesRef = firestore.collection('messages');
   const {currentUser} = useContext(AuthContext)
   const classes = useStyles();
   const [formValue, setFormValue] = React.useState("");
-  const [formats, setFormats] = React.useState(() => ['italic']);
-  
-  const handleFormat = (event, newFormats) => {
-    setFormats(newFormats);
-  };
 
   const handleSendMsg = async (e) =>{
     e.preventDefault()
@@ -77,53 +55,32 @@ export default function MessageForm() {
     })
     setFormValue('');
   }
+  
+  const handleKeypress = e => {
+    //it triggers by pressing the enter key
+  if (e.keyCode === 13) {
+    handleSendMsg();
+  }
+};
 
   const tempName = "Message";
 
   return (
     <>
-    <form className={classes.root} autoComplete="off" onSubmit={handleSendMsg}>
+    <form className={classes.root} autoComplete="off" onSubmit={handleSendMsg} >
       <Paper elevation={0} className={classes.paper}>
         <TextField
           id="outlined-multiline-static"
           label={`Send a ${tempName}`}
           value={formValue}
           onChange={(e) => setFormValue(e.target.value)}
-          multiline
-          fullWidth
+          onKeyPress={handleKeypress}
           rows={2}
           variant="outlined"
         />      
         <Button type="submit" disabled={!formValue} variant="contained" color="primary"className={classes.button}>Send<SendIcon/></Button>
       </Paper>
-      <StyledToggleButtonGroup
-        size="small"
-        value={formats}
-        onChange={handleFormat}
-        aria-label="text formatting"
-      >
-
-    <ToggleButton value="bold" aria-label="bold">
-      <FormatBoldIcon />
-    </ToggleButton>
-    <ToggleButton value="italic" aria-label="italic">
-      <FormatItalicIcon />
-    </ToggleButton>
-    <ToggleButton value="underlined" aria-label="underlined">
-      <FormatUnderlinedIcon />
-    </ToggleButton>
-    <span>  </span>
-    <span>  </span>
-    <ToggleButton value="emoji" aria-label="emoji">
-      <EmojiEmotionsIcon />
-    </ToggleButton><ToggleButton value="code" aria-label="code">
-      <CodeIcon />
-    </ToggleButton>
-    <ToggleButton value="image" aria-label="image" >
-      <AttachFileIcon />
-    </ToggleButton> 
-  </StyledToggleButtonGroup>
-    </form>
+     </form>
     </>
   );
 }
