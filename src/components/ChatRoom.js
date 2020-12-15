14d229d701f3582e8504db3ffa81af1103f51f46
pvahanian <51976194+pvahanian@users.noreply.github.com>
2020-12-14 20:49:00 -0600

    Fixed Navbar

---
+++ src/components/ChatRoom.js
@@ -0,0 +1,25 @@
+import ChannelBar from './ChannelBar.js'
+import MainChat from './MainChat.js'
+import MessageForm from './MessageForm'
+import NavBar from './Navbar'
+import '../App.css';
+
+const Chatroom = () => {
+    return (
+        <>
+        <div className="marginLeftAdjustment">
+            <div className="navbar">
+                <NavBar /> 
+            </div>
+            <div className="mainchat">
+                <MainChat />
+            </div>
+            <div className="messagebar">
+                <MessageForm />
+            </div>
+        </div>
+            <ChannelBar />
+        </>
+    )
+}
+export default Chatroom
